5b0301694f6ff57d0b96fa99bb6185f80ff9fb9a
joyqat <joyqat@users.noreply.github.com>
2018-10-29 15:46:21 +0800

    Component: add helper functions

--- flawdetector/inc/component/common.h
+++ flawdetector/inc/component/common.h
@@ -21,6 +21,18 @@ inline T subset(const T& in, int lower, int upper)
     return std::move(ret);
 }
 
+template <typename T>
+inline T limitUp(const T& in, const T& upper)
+{
+    return in > upper ? upper : in;
+}
+
+template <typename T>
+inline T limitDown(const T& in, const T& lower)
+{
+    return in < lower ? lower : in;
+}
+
 }
 
 #endif // COMMON_H
